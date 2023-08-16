1d15c491947c1a80ae6fcabc10837072254076f6
andrewssteven <andrewssteven151@gmail.com>
2023-08-16 23:56:41 +0100

    camon move

---
+++ Camon15ter/AlX/AirBnB_clone_v3/README.md
@@ -0,0 +1,1 @@
+this is a readme file for airbnb version 3
---
+++ Camon15ter/playground/1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - prints name and ask for correct
+ * answer
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, a;
+	for (x = 1; x <= 03; x++)
+	{
+	printf("Damilola o!\n");
+	}
+	printf("e me elo ni mo pe e\n");
+	printf("o le dami lohun abi : \n");
+	scanf("%d", &a);
+	if (a == 3)
+{
+	printf("ko oju ma ise e o\n");
+}
+	else
+{
+	printf("mo ti mo pe o ni gbo oro\n");
+}
+return (0);
+}
